3b6b25049417120cd1842a41ae104584f179626d
Alex Corre <alexcorre513@gmail.com>
2013-10-07 19:06:07 -0700

    Basic event bindings

--- js/main.js
+++ js/main.js
@@ -2,6 +2,67 @@
 // Main
 // =========
 
+/**
+ * @module Helpers
+ */
+var Helpers = {
+
+	/**
+	 * @method hasNoActiveInputs
+	 *
+	 * Returns true if there are not currently active elements
+	 */
+	hasNoActiveInputs: function() {
+		return document.activeElement === document.body;
+	}
+
+};
+
+/**
+ * @module App
+ * 
+ * Main application module for listening to key events and
+ * communicating with DOM reader.  Keeps current state.
+ */
+var App = {
+	
+	/**
+	 * @method initialize
+	 *
+	 * Perform app initialization on page load
+	 */
+	initialize: function() {
+		console.log('initializing');
+	},
+
+	/**
+	 * @method keyPressed
+	 *
+	 * Base Handler for key press event
+	 */
+	keyPressed: function(event) {
+		if (Helpers.hasNoActiveInputs()) {
+			// J
+			if (event.keyCode === 106) {
+				console.log('J');
+			}
+
+			if (event.keyCode === 107) {
+				console.log('K');
+			}
+		}
+	}
+
+};
+
+/**
+ * Initialization
+ */
 $(document).ready(function() {
-  console.log('loading this shiz');
+  App.initialize();
+
+  $(window).on('keypress', function(e) {
+  	App.keyPressed(e);
+  });
+
 });
